ddd6c02bb8c5ffc931f962b9223bb129d8f1a871
kenny <kenny.luque.t@uni.pe>
2022-07-27 09:39:19 -0500

    feat[nodes] : start component

---
+++ src/components/nodes/Start.vue
@@ -0,0 +1,27 @@
+<template>
+  <nodeHeader title="Start" />
+</template>
+
+<script>
+import nodeHeader from './nodeHeader.vue';
+
+import { getCurrentInstance, defineComponent, onMounted, ref } from 'vue';
+
+export default defineComponent({
+  name: 'Start',
+  components: {
+    nodeHeader
+  },
+  setup() {
+    
+    
+
+    
+  }
+
+
+});
+</script>
+
+<style>
+</style>
