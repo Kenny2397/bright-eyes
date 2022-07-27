f3e0659442fc9d3bfae8e0d01295f47db972f6ff
kenny <kenny.luque.t@uni.pe>
2022-07-27 09:37:46 -0500

    feat[nodes] : assignation component

---
+++ src/components/nodes/Assignation.vue
@@ -0,0 +1,69 @@
+<template>
+
+    <div>
+        <nodeHeader title="Assignation" />
+        <div>
+            <!-- <label for="variable"></label> -->
+            <input name="variable" type="text" v-model="variableName" @change=""
+                placeholder="Ingresa el nombre de la variable: " df-variablename>
+            <p>Variable: {{ variableName }}</p>
+            <!-- <p>Valor : {{ variableValue }}</p> -->
+        </div>
+    </div>
+
+</template>
+
+<script>
+import { ref, defineComponent  } from 'vue';
+
+import nodeHeader from './nodeHeader.vue';
+
+import { useStore } from 'vuex'
+
+
+export default defineComponent({
+    name: 'Assignation',
+    components: {
+        nodeHeader
+    },
+    setup() {
+        const variableName = ref('');
+        // const variableValue = ref(0);
+
+        // drwaflow internal
+        // const el = ref(null);
+        // const nodeId = ref(0);
+        // let df = null
+        // const dataNode = ref({});
+
+        // df = getCurrentInstance().appContext.config.globalProperties.$df.value;
+
+        // const updateNameVariable = (value) => {
+        //     dataNode.value.data.variableName = value;
+        //     df.updateNodeDataFromId(nodeId.value, dataNode.value);
+        // };
+
+        // onMounted( async () => { 
+        //     nextTick()
+        //     nodeId.value = el.value.parentElement.parentElement.id.slice(5)
+        //     dataNode.value = df.getNodeFromId(nodeId.value)
+
+        //     // guardar valores
+        //     variableValue.value = dataNode.value.data.variableValue;
+        //     variableName.value = dataNode.value.data.variableName;
+        // });
+
+        return {
+            
+            
+            variableName,
+            // variableValue,
+
+            
+        };
+    }
+});
+</script>
+
+<style>
+</style>
